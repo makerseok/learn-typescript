049313bf719f8897e9679cb1a9e23bc165260c5e
makerseok <maker7788@gmail.com>
2022-06-13 08:48:24 +0900

    class 사용

---
+++ class-note/class.js
@@ -0,0 +1,18 @@
+function PersonF(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+let 함수로만든사람 = new PersonF('함수', 7);
+
+class Person {
+  constructor(name, age) {
+    console.log('생성되었습니다~');
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let 덕팔 = new Person('덕팔', 68);
+console.log(덕팔);
+console.log(함수로만든사람);
